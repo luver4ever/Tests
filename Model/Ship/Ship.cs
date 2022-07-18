885be83bcdc48462a3e2dbeb5959f2e678524f82
luver4ever <danil.testov@gmail.com>
2022-07-18 14:00:05 +0600

    Refactor and add Dirs

---
+++ Model/Ship/Ship.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ship : Transformable
+{
+    public Ship(Vector2 position) : base(position)
+    {
+
+    }
+
+    public void MovePosition(Vector2 startPoint, Vector2 endPoint, float deltaTime, float moveTime = 2f)
+    {
+        float duration = 0;
+
+        while (duration < 1)
+        {
+            Position = Vector2.Lerp(startPoint, endPoint, duration);
+
+            duration += deltaTime / moveTime;
+        }
+
+    }
+}
